368db159036468a0e5083b3d5c683336d861dfde
Simon Weller <s.meierbeck@gmail.com>
2020-05-25 22:09:50 +0200

    avoid adding and removing event listeners by attaching a single toggleTimer function

--- index.js
+++ index.js
@@ -12,40 +12,31 @@
   document.querySelector("#break-length").textContent = breakLength;
   document.querySelector("#session-length").textContent = sessionLength;
 
-  document.querySelector("#start_stop").addEventListener("click", startTimer);
+  document.querySelector("#start_stop").addEventListener("click", toggleTimer);
   document.querySelector("#controls").addEventListener("click", changeTimes);
   document.querySelector("#reset").addEventListener("click", reset);
 
-  function startTimer() {
-    countDownTimer = setInterval(function countDown() {
-      remainingSeconds--;
-
-      if (remainingSeconds < 0) {
-        switchMode();
-      } else {
-        render();
-      }
-    }, 1000);
-
-    counterRunning = true;
-
-    // Toggle event listeners
-    document
-      .querySelector("#start_stop")
-      .removeEventListener("click", startTimer);
-    document.querySelector("#start_stop").addEventListener("click", stopTimer);
+  function toggleTimer() {
+    counterRunning ? stopTimer() : startTimer();
   }
 
   function stopTimer() {
     clearInterval(countDownTimer);
-
     counterRunning = false;
+  }
 
-    // Toggle event listeners
-    document
-      .querySelector("#start_stop")
-      .removeEventListener("click", stopTimer);
-    document.querySelector("#start_stop").addEventListener("click", startTimer);
+  function startTimer() {
+    countDownTimer = setInterval(countDown, 1000);
+    counterRunning = true;
+  }
+
+  function countDown() {
+    remainingSeconds--;
+
+    if (remainingSeconds < 0) {
+      switchMode();
+    }
+    render();
   }
 
   function reset() {
@@ -139,7 +130,6 @@
       remainingSeconds = sessionLength * 60;
     }
     document.querySelector("#timer-label").textContent = currentMode;
-    render();
   }
 
   function getFillColor() {
